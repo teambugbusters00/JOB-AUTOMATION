73058b5495432386129a4e84f93904aa24600583
vijay jangid <186843345+teambugbusters00@users.noreply.github.com>
2026-09-15 03:13:55 +0530

    Add production collector package

---
+++ job_agent/collectors/__init__.py
@@ -0,0 +1,6 @@
+from .himalayas import collect_himalayas
+from .greenhouse import collect_greenhouse
+from .lever import collect_lever
+from .ashby import collect_ashby
+
+__all__ = ["collect_himalayas", "collect_greenhouse", "collect_lever", "collect_ashby"]
